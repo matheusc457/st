b1b079c1a319a117014c37b60253f2304747a952
Matheus C. <245040149+matheusc457@users.noreply.github.com>
2026-08-25 16:26:18 -0300

    edit font pixel size

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "CaskaydiaCove Nerd Font Mono:size:12:pixelsize=19:antialias=true:autohint=true";
-static char *font = "JetBrainsMono Nerd Font Mono:pixelsize=19:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
